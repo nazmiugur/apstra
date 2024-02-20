3366e42f341638536efed5b033376c36a5c45a44
alperay burak keleş <alperay.keles@sekom.com.tr>
2024-02-20 14:52:02 +0300

    modify if listing, add lacp mode set-unset

--- filter_plugins/consolidate_sw_if_with_aep_if.py
+++ filter_plugins/consolidate_sw_if_with_aep_if.py
@@ -16,6 +16,16 @@ class FilterModule(object):
          print(aep_portname)
       return retval
 
+    def getRackIDandLabel(self, sw_name, aep_if_list):
+       retval=[]
+       for aep_if in aep_if_list:
+          if(sw_name == aep_if['sw_label']):
+             retval.append(aep_if['rack_id'])
+             retval.append(aep_if['rack_label'])
+             break
+       return retval
+
+
 
 
     def findIndex(self, sw_if_list, portname, sw_id):
@@ -60,6 +70,40 @@ class FilterModule(object):
                 sw_if[indexes[0]][indexes[1]].update({"interface_speed":speed})
              sw_if[indexes[0]][indexes[1]].update({"interface_aep_id": aep_if['interface_aep_id']})
              sw_if[indexes[0]][indexes[1]].update({"interface_aep_label": aep_if['interface_aep_label']})
-
+       for switch in sw_if:
+           for port in switch:
+              rackinfo=self.getRackIDandLabel(port['sw_label'], aep_if_list)
+              if(len(rackinfo)==2):
+                port.update({"rack_id": rackinfo[0]})
+                port.update({"rack_label": rackinfo[1]})
+              else:
+                port.update({"rack_id": ""})
+                port.update({"rack_label": ""})
+              port.update({"connected_to_id": ""})
+              if 'ifsinfo' in value:
+                speed=self.findPortSpeed(aep_if['interface_aep_id'], ifsinfo)  
+              port.update({"connected_to_type": ""})
+              port.update({"connected_to_label": ""})
+       if 'ifsinfo' in value:
+          ifsinfo=value['ifsinfo']
+          for iface in ifsinfo['items']:
+             portname=iface['iface']['if_name']
+             sw_id=iface['system']['id']
+             sw_name=iface['system']['label']
+             connected_to_id=iface['remote_system']['id']
+             connected_to_type=iface['remote_system']['type']
+             connected_to_label=iface['remote_system']['label']
+             indexes=self.findIndex(sw_if,portname, sw_id)
+             if(len(indexes)==2):
+                rackinfo=self.getRackIDandLabel(sw_name, aep_if_list)
+                if(len(rackinfo)==2):
+                   sw_if[indexes[0]][indexes[1]].update({"rack_id": rackinfo[0]})
+                   sw_if[indexes[0]][indexes[1]].update({"rack_label": rackinfo[1]})
+                if(connected_to_id !=""):
+                   speed=connected_to_id=iface['link']['speed'].split("G")[0]
+                   sw_if[indexes[0]][indexes[1]].update({"speed": speed})
+                sw_if[indexes[0]][indexes[1]].update({"connected_to_id": connected_to_id})
+                sw_if[indexes[0]][indexes[1]].update({"connected_to_type": connected_to_type})
+                sw_if[indexes[0]][indexes[1]].update({"connected_to_label": connected_to_label})
        return sw_if
            
